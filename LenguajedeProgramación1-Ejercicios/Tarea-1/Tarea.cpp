511835345efe968cb5192f8ebe287e02df4206d3
Marvin Cruz <cruzmarvin238@gmail.com>
2022-02-02 12:17:30 -0600

    Funcion suma

---
+++ LenguajedeProgramación1-Ejercicios/Tarea-1/Tarea.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    int b = 0;
+    int suma = 0;
+
+    cout << "Ingrese el valor de A: ";
+    cin >> a;
+    cout << "Ingrese el valor de B: ";
+    cin >> b;
+    suma = a+b;
+
+    cout << "La suma de a + b: " << suma;
+
+    return 0;
+}
