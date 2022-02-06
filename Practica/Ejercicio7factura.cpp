3e9099f2bec28cefe2632b63158d1405d9f35f71
Marvin Cruz <cruzmarvin238@gmail.com>
2022-02-05 21:21:54 -0600

    ds

---
+++ Practica/Ejercicio7factura.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // datos de entrada
+    double subtotal = 0;
+    double total = 0;
+    double imp = 0.15;
+
+    cout<< "Ingrese el subtotal de la factura";
+    cin >> subtotal;
+     
+    // proceso
+    total = subtotal + (subtotal*imp);
+
+    // salida
+    cout <<end;
+    cout << "Total a pagar es: "<<total;
+    return 0;
+}
