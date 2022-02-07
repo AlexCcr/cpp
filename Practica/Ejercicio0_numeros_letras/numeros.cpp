53cdac501aea692bcb4783fd7aee26082eabaa1a
Marvin Cruz <cruzmarvin238@gmail.com>
2022-02-07 17:44:49 -0600

    Numeros

---
+++ Practica/Ejercicio0_numeros_letras/numeros.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numero = 'A';
+    int numero2 = 'B';
+    int numero3 = 'a';
+    int numero4 = 'b';
+    int numero5 = '1';
+    int numero6 = ' ';
+    cout << "Letras mayusculas:" <<endl;
+    cout << "El valor de 'A' "<< numero <<endl;
+    cout << "El valor de 'B' "<< numero2<<endl;
+    cout << "Letras minusculas:" <<endl;
+    cout << "El valor de 'a' "<< numero3<<endl;
+    cout << "El valor de 'b' "<< numero4<<endl;
+    cout << "Numeros:" <<endl;
+    cout << "El valor de '1' "<<numero5<<endl;
+    cout << "Espacio:" <<endl;
+    cout << "El valor de ' ' "<<numero6<<endl;
+    return 0;
+}
