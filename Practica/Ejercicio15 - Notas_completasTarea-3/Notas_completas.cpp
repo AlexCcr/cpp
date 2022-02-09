904c51dd61bcf31672670b65c4ea42101cc38e89
Marvin Cruz <cruzmarvin238@gmail.com>
2022-02-08 22:19:47 -0600

    cambios

--- Practica/Ejercicio15 - Notas_completasTarea-3/Notas_completas.cpp
+++ Practica/Ejercicio15 - Notas_completasTarea-3/Notas_completas.cpp
@@ -13,23 +13,55 @@ int main(int argc, char const *argv[])
         return 0;
     }
 
-   /* 100	S(A++)
-95	A+      91
-90	A       86
-85	A-      81
-80	B+      76
-75	B       71
-70	B-      66
-65	        61
-60	C       56
-55	C-      51
-50	D+      46
-45	D       41
-40	D-      36
-35	E+      31
-30	E       29
-25	E-      21
-20	F+      16
+if(nota >= 96 && nota <= 100){
+    cout<< "Obtuvo una calificación de S(A++)";
+}
+if(nota >= 91 && nota <= 95){
+    cout<< "Obtuvo una calificación de A++";
+}
+if(nota >= 86 && nota <= 90){
+    cout<< "Obtuvo una calificación de A";
+}
+if(nota >= 81 && nota <= 85){
+    cout<< "Obtuvo una calificación de A-";
+}
+if(nota >= 76 && nota <= 80){
+    cout<< "Obtuvo una calificación de B+";
+}
+if(nota >= 71 && nota <= 75){
+    cout<< "Obtuvo una calificación de B";
+}
+if(nota >= 66 && nota <= 70){
+    cout<< "Obtuvo una calificación de B-";
+}
+if(nota >= 61 && nota <= 65){
+    cout<< "Obtuvo una calificación de C";
+}
+if(nota >= 56 && nota <= 60){
+    cout<< "Obtuvo una calificación de C-";
+}
+if(nota >= 51 && nota <= 55){
+    cout<< "Obtuvo una calificación de C-";
+}
+
+if(nota >= 46 && nota <= 50){
+    cout<< "Obtuvo una calificación de D+";
+}
+if(nota >= 41 && nota <= 45){
+    cout<< "Obtuvo una calificación de D";
+}
+if(nota >= 36 && nota <= 40){
+    cout<< "Obtuvo una calificación de D-";
+}
+if(nota >= 31 && nota <= 35){
+    cout<< "Obtuvo una calificación de E+";
+}
+if(nota >= 26 && nota <= 30){
+    cout<< "Obtuvo una calificación de E";
+}
+if(nota >= 21 && nota <= 25){
+    cout<< "Obtuvo una calificación de E-";
+}
 if(nota >= 16 && nota <= 20){
     cout<< "Obtuvo una calificación de F+";
 }
@@ -46,6 +78,5 @@ if(nota >=   0 && nota <= 5){
 if(nota == 0){
     cout<< "Obtuvo una calificación de F--";
 }
-*/
     return 0;
 }
