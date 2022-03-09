adbcdfcca6918671455d170f75befb5ea21feac7
Marvin Cruz <cruzmarvin238@gmail.com>
2022-03-08 20:41:08 -0600

    fin

--- Practica/Ejercicio45 - Tarea-punto-de-venta/factura.cpp
+++ Practica/Ejercicio45 - Tarea-punto-de-venta/factura.cpp
@@ -2,18 +2,24 @@
 
 using namespace std;
 
+double total;
 double subtotal;
+double isv;
 string listaProductos;
 
 void agregarProducto(string descripcion, int cantidad, double precio)
 {   
     listaProductos = listaProductos + descripcion + '\n';
     subtotal = subtotal + (cantidad*precio);
+    isv = subtotal * 0.15;
+    total = subtotal+isv;
 }
 
 void imprimirfactura()
 {
     system ("cls");
+
+    
     cout << "*******"<< endl;
     cout << "FACTURA"<< endl;
     cout << "*******"<< endl <<endl;
@@ -23,6 +29,13 @@ void imprimirfactura()
 
     cout << endl;
     cout << "SUBTOTAL: "<<subtotal;
+    cout << endl;
+
+    cout << endl;
+    cout << "TOTAL: "<<total;
+    cout << endl;
+    cout << endl;
+    cout << "IMPUESTO SOBRE VENTAS (15%): "<<total;
     cout << endl;
     system ("pause");
 }
